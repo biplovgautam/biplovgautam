29825a32885367541927f8b5aa9519b3395681dd
biplovgautam <134443854+biplovgautam@users.noreply.github.com>
2023-07-07 03:38:56 +0545

    Update index.js

--- index.js
+++ index.js
@@ -15,29 +15,3 @@ function scrollHide() {
   }
   lastScrollTop = st <= 0 ? 0 : st;
 }
-
-
-let searchbtn = document.querySelector('.searchbtn');
-let closebtn = document.querySelector('.closebtn');
-let searchbox = document.querySelector('.searchbox');
-let navigation = document.querySelector('.navigation');
-let menulog = document.getElementById(".menulog1");
-let header = document.getElementById(".header1");
-searchbtn.onclick = function(){
-  closebtn.classList.add('active');
-  searchbox.classList.add('active');
-  searchbtn.classList.add('active');
-  menulog.classList.add('hide');
-}
-closebtn.onclick = function(){
-  closebtn.classList.remove('active');
-  searchbox.classList.remove('active');
-  searchbtn.classList.remove('active');
-  menulog.classList.remove('hide')
-}
-menulog.onclick = function(){
-  header.classList.toggle('open');
-  searchbox.classList.remove('active');
-  searchbtn.classList.remove('active');
-  menulog.classList.remove('hide')
-}
